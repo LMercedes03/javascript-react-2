641a56f909010c46f7f7dabb7b0d151925d98c0b
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-05-23 16:02:09 -0400

    Complete ex1

--- src/Student8/Ex1/DateFetcher.js
+++ src/Student8/Ex1/DateFetcher.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { useState } from 'react'
 
 export const DateFetcher = () => {
-    const [date, setDate]= useState("Unknown")
-    <div className= "Main">
+  const [date, setDate]= useState("Unknown")
+   
+  return (
+    <div className="Main">
+      <button onClick={()=>setDate(new Date().toUTCString()) }>Push </button>
+      UTC date: {date}
     </div>
-    return (
-        <div>DateFetcher</div>
   )
 }
